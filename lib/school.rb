8dcb73d27f9c7a53855d8ec8c0d2e33e43f56173
februar-y <jarmah241@gmail.com>
2020-10-27 11:12:06 -0400

    completed

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,43 @@
 # code here!
+
+class School
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def name
+        @name
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        @name = name
+        @grade = grade
+        # If the key @roster[@grade] already exist shovel value else
+        # create key for grade and the shovel value to it
+        if @roster[@grade]
+            @roster[@grade] << @name
+        else 
+            @roster[@grade] = []
+            @roster[@grade] << @name
+        end
+    end
+
+    def grade(grade_num)
+        @roster[grade_num]
+    end
+
+    def sort
+        @roster = @roster.sort.to_h
+        @roster.each do |key, value|
+            @roster[key] = value.sort
+        end
+        @roster
+    end
+
+end
